8bc96b89875dbe191191b88f7d12d742d44cd135
Andy <batuure15@gmail.com>
2023-11-08 19:09:44 -0500

    model created to guess positions

--- utils/helpers.py
+++ utils/helpers.py
@@ -14,3 +14,19 @@ def find_play_type(plays_df,phrase:str = 'fumbl',disp=False):
         print('Number of matches for {}: {}'.format(phrase,len(matches)))
         
     return matches
+
+
+#adapted from kaggle.com
+def reverse_play_direction(df):
+        result_df = df.copy(deep=True)
+        result_df["o"] = result_df["o"].apply(lambda o: 360 - o)
+        result_df["o"] = result_df["o"].replace(360, 0)
+        
+        result_df["dir"] = result_df["dir"].apply(lambda dir: 360 - dir)
+        result_df["dir"] = result_df["dir"].replace(360, 0)
+        
+        result_df["x"] = result_df["x"].apply(lambda x: 120 - x)
+        
+        result_df["y"] = result_df["y"].apply(lambda y: 160/3 - y)
+        
+        return result_df
